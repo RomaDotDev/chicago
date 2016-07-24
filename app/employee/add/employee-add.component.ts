3281c8403969fd17562021804e98ddc42b0b50a3
Roman Ilyushin <irsick@gmail.com>
2016-07-24 14:20:04 -0400

    Employeed add form and validation

--- app/employee/add/employee-add.component.ts
+++ app/employee/add/employee-add.component.ts
@@ -1,6 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+import { Subscription } from 'rxjs/Subscription';
+import { EmployeeService } from '../employee.service';
+import { Employee } from '../employee';
 
 @Component({
-    templateUrl: 'app/employee/add/employee-add.html'
+    templateUrl: 'app/employee/add/employee-add.html',
+    directives: [ NgClass ]
 })
-export class EmployeeAddComponent { }
+
+// TODO: check if ID is available
+// TODO: do not allow negative numbers
+// TODO: department dropdown
+
+export class EmployeeAddComponent implements OnInit, OnDestroy {
+    @ViewChild('addEmployeeForm') addEmployeeForm:any;
+
+    constructor(private employeeService: EmployeeService){}
+
+    private submitSubscription:Subscription;
+    private isSubmitted:boolean;
+    private formModel:Object;
+
+    ngOnInit(){
+        this.isSubmitted = false;
+        this.formModel = {};
+    }
+
+    ngOnDestroy(){
+        if (this.submitSubscription){
+            this.submitSubscription.unsubscribe();
+        }
+    }
+
+    onSubmit() {
+        this.isSubmitted = true;
+
+        if (this.addEmployeeForm.form.valid){
+
+            if (this.submitSubscription){
+                this.submitSubscription.unsubscribe();
+            }
+            this.submitSubscription = this.employeeService.addEmployee(this.mapFormData(this.formModel))
+                .subscribe(result => console.log(result));
+        }
+    }
+
+    // normalize data for API format
+    // uppercase values to match existing db representation
+    private mapFormData(formObject:Object):Employee{
+        return {
+            'department': this.formModel['department'].toUpperCase(),
+            'employee_annual_salary': this.formModel['salary'],
+            'id': this.formModel['id'],
+            'job_titles': this.formModel['title'].toUpperCase(),
+            'name': `${this.formModel['firstName'].toUpperCase()} ${this.formModel['lastName'].toUpperCase()}`
+        }
+    }
+
+}
