3cc4c51c43c9d6583cf847b21500450d0909c7e1
Roman Ilyushin <irsick@gmail.com>
2016-07-25 00:06:50 -0400

    Response alert and spinner for add form

--- app/employee/add/employee-add.component.ts
+++ app/employee/add/employee-add.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 import { NgClass } from '@angular/common';
-
 import { Subscription } from 'rxjs/Subscription';
+
 import { EmployeeService } from '../employee.service';
+import { SpinnerComponent } from '../../shared/spinner/spinner.component';
 import { Employee } from '../employee';
 
 @Component({
     templateUrl: 'app/employee/add/employee-add.html',
-    directives: [ NgClass ]
+    directives: [ NgClass, ROUTER_DIRECTIVES, SpinnerComponent ]
 })
 
 // TODO: check if ID is available
 // TODO: do not allow negative numbers
 // TODO: department dropdown
+// TODO: success message
 
 export class EmployeeAddComponent implements OnInit, OnDestroy {
     @ViewChild('addEmployeeForm') addEmployeeForm:any;
@@ -21,11 +24,15 @@ export class EmployeeAddComponent implements OnInit, OnDestroy {
 
     private submitSubscription:Subscription;
     private isSubmitted:boolean;
+    private isLoading:boolean;
     private formModel:Object;
+    private response:Object;
 
     ngOnInit(){
+        this.isLoading = false;
         this.isSubmitted = false;
         this.formModel = {};
+        this.response = {};
     }
 
     ngOnDestroy(){
@@ -42,8 +49,12 @@ export class EmployeeAddComponent implements OnInit, OnDestroy {
             if (this.submitSubscription){
                 this.submitSubscription.unsubscribe();
             }
+            this.isLoading = true;
             this.submitSubscription = this.employeeService.addEmployee(this.mapFormData(this.formModel))
-                .subscribe(result => console.log(result));
+                .subscribe(result => {
+                    this.isLoading = false;
+                    this.response = result;
+                });
         }
     }
 
